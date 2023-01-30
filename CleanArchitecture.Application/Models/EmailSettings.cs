f5d23717139e3cd27770d297bb4ab3a7036b29f2
Juan Carlos <jcmamani@fassil.com.bo>
2023-01-29 20:19:27 -0400

    implement patern mediator and cqrs in projects

---
+++ CleanArchitecture.Application/Models/EmailSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.Application.Models
+{
+    public class EmailSettings
+    {
+        public string ApyKey { get; set; }
+        public string FromAddress { get; set; }
+        public string FromName { get; set; }
+    }
+}
